dbb288fd8d7256de4c97e46e966ae967b5d1097e
Sulav Acharya <asulav@gmail.com>
2023-02-15 13:53:13 -0500

    init: initial playwrite item

---
+++ tests/example.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('click link that doesnt exists', async ({ page }) => {
+  await page.getByRole('button', { name: 'Node.js' }).hover();
+  await page.getByRole('navigation').getByRole('link', { name: 'Python' }).click();
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects the URL to contain intro.
+  await expect(page).toHaveURL(/.*intro/);
+});
